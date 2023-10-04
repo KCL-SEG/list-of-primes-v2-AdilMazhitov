cff089d816f098861bda749738b2389c7e6bc850
Adil Mazhitov <k21117945@kcl.ac.uk>
2023-10-05 02:30:36 +0600

    Add a function of valuError to primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,20 @@
 
 def primes(number_of_primes):
     list = []
+    primeNumber = 2
+    if (number_of_primes) < 1:
+        raise ValueError(f"{number_of_primes} should have been a positive number.")
+    while (len(list) < int(number_of_primes)):
+        counter = 0
+        for i in range(1, primeNumber+1):
+            if primeNumber % i == 0:
+                counter += 1
+        
+        if (counter == 2):
+            list.append(primeNumber)
+
+        primeNumber += 1 
+
     return list
+
+primes(10)
